0e5b4fb31a1b28e08c1e7c54b7cd8a9feed6762b
NervousK <quentin.chaunier@gmail.com>
2017-12-21 23:16:37 +0100

    Context addaed, encore decode functions, work in progress... probem with utf-8 and bytes encoding

--- prime_tests.py
+++ prime_tests.py
@@ -1,5 +1,6 @@
 
 import random
+import json
 from math import gcd
 from math import log
 from math import floor
@@ -213,10 +214,14 @@ def Lucas_test(n):
     return True
 
 def is_square(n):
+
+    def bitlength(x):
+        return int(log(n, 2)) + 1
+
     x = int(n)
     if x == 0:
-        return 0
-    a, b = divmod(x.bitlength(), 2)
+        return False
+    a, b = divmod(bitlength(x), 2)
     s = 2**(a+b)
     while True:
         y = (s+x //s) >>1
@@ -224,7 +229,7 @@ def is_square(n):
             if s*s==n:
                 return s
             else:
-                return True
+                return False
         s = y
 
 def Baillie_PSW(n, k, B):
@@ -235,8 +240,12 @@ def Baillie_PSW(n, k, B):
 
     #check des premiers primes ou autre crible
     for i in list(range(3, B+1, 2)):
-        if n % i:
-            return n == i
+        if n % i == 0:
+            if n == i:
+                return True
+            else:
+                return False
+
 
     if not Miller_Rabin_test(n, k):
         return False
@@ -260,3 +269,272 @@ def Pollard(n, B, k):
             return False
     else:
         return True
+
+
+if __name__ == '__main__':
+
+    def pick_candidate(length):
+        range_start = (2**(length-1))
+        range_end = (2**length)-1
+        candidate = random.randint(range_start, range_end)
+
+        return candidate
+
+    def pick_prime(length=64):
+        prime= 0
+        while prime == 0:
+            candidate = pick_candidate(length)
+            # print(candidate)
+            if Baillie_PSW(candidate, 2, 800):
+                # print('Success')
+                prime = candidate
+            # else:
+            #     print('Fail')
+        return prime
+
+    def is_smooth(p, k=2, B=800):
+        if p % 2 != 0:
+            q = (p-1)//2
+            if Baillie_PSW(q, k, B):
+                return False
+            else:
+                return True
+        else:
+            return True
+
+    def find_generator(p):
+        q = (p-1)//2
+        h = 0
+        while h == 0:
+            candidate = random.randint(2, p-1)
+            # print('candidate: '+str(candidate))
+            # print('p: '+str(p))
+            # print('(p-1)/q: '+str((p-1)/q))
+            if pow(candidate, (p-1)//q, p) !=1:
+                h = candidate
+        return h
+
+
+    def generate_keys(length=8):
+        length*=8*2
+        p = pick_prime(length)
+        alpha1 = find_generator(p)
+        alpha2 = find_generator(p)
+        while alpha1 == alpha2:
+            alpha2 = find_generator(p)
+        x1 = random.randint(2, p-1)
+        x2 = random.randint(2, p-1)
+        y1 = random.randint(2, p-1)
+        y2 = random.randint(2, p-1)
+        w = random.randint(2, p-1)
+        X = (pow(alpha1, x1, p)*pow(alpha2, x2, p))%p
+        Y = (pow(alpha1, y1, p)*pow(alpha2, y2, p))%p
+        W = pow(alpha1, w, p)
+
+        key_data = {'public key':
+                               {'p': p,
+                                'alpha1': alpha1,
+                                'alpha2': alpha2,
+                                'X':X,
+                                'Y':Y,
+                                'W':W,
+                                },
+                           'private key':
+                               {'x1': x1,
+                                'x2': x2,
+                                'y1': y1,
+                                'y2': y2,
+                                'w': w
+                                }
+                           }
+
+        with open('keys.json', 'w') as keydatafile:
+            json.dump({'Alice': key_data}, keydatafile,indent=4, sort_keys=True)
+
+        return key_data
+
+    def byte_len(x):
+        s = bin(x)
+        s = s.lstrip('-0b')
+        return (len(s)//8)+1
+
+    def encode_block(block,p, b, B1, B2, W):
+
+        block = int.from_bytes(block, byteorder='big')
+        c = (pow(W, b, p)*block)%p
+        cyfered_block = bytearray(c.to_bytes((c.bit_length()//8)+1, byteorder='big'))
+        print(cyfered_block)
+        with open('cyfer.json', 'r+') as cyferdatafile:
+            data = cyferdatafile.read()
+            if len(data)>0:
+                current_dict = json.loads(data)
+                current_dict['Cyfered Block']= str(cyfered_block)
+            else:
+                current_dict = {'Cyfered Block': str(cyfered_block)}
+
+            cyferdatafile.seek(0)
+            json.dump(current_dict,
+                      cyferdatafile,indent=4, sort_keys=True)
+            cyferdatafile.truncate()
+
+        return cyfered_block
+
+    def encode_message(m, public_key, block_size):
+        p = public_key['p']
+        alpha1 = public_key['alpha1']
+        alpha2 = public_key['alpha2']
+        X = public_key['X']
+        Y = public_key['Y']
+        W = public_key['W']
+        b = random.randint(2, p-1)
+        B1 = pow(alpha1, b, p)
+        B2 = pow(alpha2, b, p)
+
+        m_bytes = bytearray(str(m).encode('utf-8'))
+        m_blocks = []
+        for block_number in list(range(len(m_bytes)//block_size)):
+            m_block = bytearray()
+            for i in list(range(block_size)):
+                m_block.append(m_bytes[block_number*8+i])
+            m_blocks.append(m_block)
+
+        m_block = m_bytes[len(m_bytes)//block_size:]
+        while len(m_block) < block_size:
+            m_block.append(b'x00')
+
+        cyfered_text = bytearray()
+
+        for block in m_blocks:
+            cyfered_block = encode_block(block,p, b, B1, B2, W)
+            cyfered_text.extend(cyfered_block)
+
+
+        with open('keys.json', 'r+') as keydatafile:
+            data = keydatafile.read()
+            if len(data)>0:
+                current_dict = json.loads(data)
+                current_dict['Bob'] = {'B1': B1,
+                                  'B2': B2}
+            else:
+                current_dict = {'Bob': {'B1': B1,
+                                        'B2': B2}}
+            keydatafile.seek(0)
+            json.dump(current_dict,
+                      keydatafile,indent=4, sort_keys=True)
+            keydatafile.truncate()
+
+        with open('cyfer.json', 'r+') as cyferdatafile:
+            data = cyferdatafile.read()
+            if len(data) > 0:
+                current_dict = json.loads(data)
+                current_dict['Cyfered Text'] = str(cyfered_text)
+            else:
+                current_dict = {'Cyfered Text': str(cyfered_text)}
+            cyferdatafile.seek(0)
+            json.dump(current_dict,
+                      cyferdatafile,indent=4, sort_keys=True)
+            cyferdatafile.truncate()
+
+        return cyfered_text
+
+    def decode_bloc(block, p, B1, w):
+
+        block = int.from_bytes(block, byteorder='big')
+        m = (pow(B1, p-1-w, p) * block)% p
+        uncyfered_block = bytearray(m.to_bytes((m.bit_length()//8)+1, byteorder='big'))
+
+        with open('cyfer.json', 'r+') as cyferdatafile:
+            data = cyferdatafile.read()
+            if len(data) > 0:
+                current_dict = json.loads(data)
+                current_dict['Cyfered Block'] = str(uncyfered_block)
+            else:
+                current_dict = {'Cyfered Block': str(uncyfered_block)}
+            cyferdatafile.seek(0)
+            json.dump(current_dict,
+                      cyferdatafile,indent=4, sort_keys=True)
+            cyferdatafile.truncate()
+
+        return uncyfered_block
+
+    def decode_message(c, private_key, public_key, block_size):
+
+        p = public_key['p']
+        B1 = public_key['B1']
+        B2 = public_key['B2']
+        w = private_key['w']
+
+        uncyfered_text = bytearray()
+
+        c_blocks = []
+        for block_number in list(range(len(c)//block_size)):
+            c_block = bytearray()
+            for i in list(range(block_size)):
+                c_block.append(c[block_number*8+i])
+            c_blocks.append(c_block)
+
+        for block in c_blocks:
+            uncyfered_block = decode_bloc(block, p, B1, w)
+            uncyfered_text.extend(uncyfered_block)
+
+        with open('cyfer.json', 'r+') as cyferdatafile:
+            data = cyferdatafile.read()
+            if len(data) > 0:
+                current_dict = json.loads(data)
+                current_dict['Unyfered Text'] = str(uncyfered_text)
+            else:
+                current_dict = {'Unyfered Text': str(uncyfered_text)}
+            cyferdatafile.seek(0)
+            json.dump(current_dict,
+                      cyferdatafile,indent=4, sort_keys=True)
+            cyferdatafile.truncate()
+
+        return uncyfered_text
+
+    def test():
+        message = 'this is the message to be cyfered by the cyfer function and lets hope it works cause its gtting late!'
+        clear_text = bytearray(str(message).encode('utf-8'))
+        with open('cyfer.json', 'r+') as cyferdatafile:
+            data = cyferdatafile.read()
+            if len(data) > 0:
+                current_dict = json.loads(data)
+                current_dict['Clear Text'] = str(clear_text)
+            else:
+                current_dict = {'Clear Text': str(clear_text)}
+            cyferdatafile.seek(0)
+            json.dump(current_dict,
+                      cyferdatafile,indent=4, sort_keys=True)
+            cyferdatafile.truncate()
+
+        generate_keys(8)
+
+        with open('keys.json') as keydatafile:
+            keys = json.loads(keydatafile.read())
+            public_key = keys['Alice']['public key']
+            cyfer = encode_message(message, public_key, 8)
+            print(cyfer)
+            with open('keys.json') as keydatafile:
+                keys = json.loads(keydatafile.read())
+                public_key['B1'] = keys['Bob']['B1']
+                public_key['B2'] = keys['Bob']['B2']
+                private_key = keys['Alice']['private key']
+                decyfer = decode_message(cyfer, private_key, public_key, 8)
+                print(decyfer)
+
+
+
+
+
+
+
+
+    # t = Baillie_PSW(37, 2, 800)
+    # print(t)
+    # t = Baillie_PSW(179425993, 2, 800)
+    # print(t)
+    # t = test(256)
+    #
+    # print(t)
+    test()
+
+
